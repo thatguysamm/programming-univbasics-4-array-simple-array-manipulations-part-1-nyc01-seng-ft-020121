4ad3dbbd7b0d01a22fa8ce76bebcd9201c36f9bc
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-12 21:07:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,7 +9,5 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   eleted_string= continents.pop
-  continents.length
 end
